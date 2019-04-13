a55c6ac89d68ee3405a32ec351c223a84bf725ea
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-04-13 02:04:50 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBotDemo
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "[YOUR TOKEN HERE]",
+                TokenType = TokenType.Bot
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+
+            await discordClient.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task OnMessageCreated(MessageCreateEventArgs e)
+        {
+            if(string.Equals(e.Message.Content, "hello", StringComparison.OrdinalIgnoreCase))
+            {
+                await e.Message.RespondAsync(e.Message.Author.Username);
+            }
+        }
+    }
+}
